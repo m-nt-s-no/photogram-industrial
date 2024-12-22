e7c8024cd90e660562961aef99c760afca64f3d1
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-22 21:45:44 +0000

    Added navbar, cdn assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "photos#index"
+  devise_for :users
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 end
